e10ff9c652f57131514f5c594027a3a421d2d1fe
DilanoG <31475184+DilanoG@users.noreply.github.com>
2018-06-05 09:26:36 +0200

    Add files via upload

---
+++ kalender-php/controller/BirthdayController.php
@@ -0,0 +1,40 @@
+<?php
+
+require(ROOT . "model/BirthdayModel.php");
+
+function index(){
+	render("home/index", array(
+		'birthdays' => getAllBirthdays()
+	));
+}
+
+function kalender(){
+	render("birthday/index", array(
+		'birthdays' => getAllBirthdays()
+	));
+}
+
+function create(){
+	render("birthday/create");
+}
+
+function delete($id){
+	deleteUser($id);
+	header("Location: /kalender-php/birthday/kalender");
+}
+
+function postEdit($id){
+	editUser($id);
+	header("Location: /kalender-php/birthday/kalender");
+}
+
+function edit($id){
+	render("birthday/edit", array(
+		"result" => getUser($id)
+	));
+}
+
+function insert(){
+	insertUser();
+	header("Location: /kalender-php/birthday/kalender");
+}
---
+++ kalender-php/view/home/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+	<h2> Welkom op mijn Kalender</h2>
+</div>
+<style>
+.remove {
+color: red;
+font-weight: bold;
+}
+</style>
